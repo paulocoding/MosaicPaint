db16900705e2fb2a4de1f739ca5be24bf490ebe1
Paulo Silva <engelik@gmail.com>
2015-12-18 19:56:48 +0000

    added jquery

--- source/js/base.js
+++ source/js/base.js
@@ -1,1 +1,16 @@
-console.log("Im working!");
+var main= function(){
+	var generateGrid = function(l){
+		l = l*l
+		for(var i=0;i<l;i++){
+			$('.my-mosaic').append(mySquare);
+		}
+	}
+	var mySquare = '<div class="square"></div>';
+	var gridLength = 10;
+	
+	
+	generateGrid(gridLength);
+	
+};
+
+$(document).ready(main);
