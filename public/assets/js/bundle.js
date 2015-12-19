9da2d6365709d45cecb21bcb43774385bfe441e6
Paulo Silva <engelik@gmail.com>
2015-12-19 20:54:04 +0000

    added gui and functionality for square size

--- public/assets/js/bundle.js
+++ public/assets/js/bundle.js
@@ -3,7 +3,9 @@ var main= function(){
 	// generates a grid with l squares on each side
 	var generateGrid = function(totalWidth, l){
 		var squareWidth=(totalWidth/l)+'px';
+		// squaring side size
 		l = l*l
+		$('.my-mosaic')[0].innerHTML="";
 		$mosaic=$('.my-mosaic');
 		for(var i=0;i<l;i++){
 			$mosaic.append(mySquare);
@@ -20,8 +22,18 @@ var main= function(){
 	// setting grid length 
 	var gridLength = 10;
 	
+	$('#square-range').change(function(){
+		$('.square-number').text(this.value);
+	});
+	
+	$('.new-mosaic').click(function(){
+		generateGrid(parseInt($('.my-mosaic').css('width')), parseInt($('#square-range')[0].value));
+	$('.square').mouseover(paintGrid);	
+	});
+	
 	generateGrid(parseInt($('.my-mosaic').css('width')), gridLength);
-	$('.square').mouseover(paintGrid);
+	
+	$('.square').mouseover(paintGrid);	
 	// end of main function
 };
 
